bb807c178f0624a313848f27e7c51b2c9ff6061d
Lőrinc19 <locics@gmail.com>
2024-09-19 09:44:52 +0200

    Text file is used

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,15 @@ namespace Employees
 {
     internal class Program
     {
+        public static List<Employer> employers = new List<Employer>();
+
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("tulajdonsagok_100sor.txt");
+
+            Employer emp =new Employer(sr.ReadLine());
+
+            Console.WriteLine(emp.Name);
         }
     }
 }
